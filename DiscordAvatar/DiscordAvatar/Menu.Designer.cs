44faddd6f109bd2efbdbf5fde7a81dbc91379651
cliq-uee <101825643+cliq-uee@users.noreply.github.com>
2022-08-05 18:44:43 -0700

    Uploaded files

---
+++ DiscordAvatar/DiscordAvatar/Menu.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace DiscordAvatar
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idField = new System.Windows.Forms.TextBox();
+            this.getAvatarButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // idField
+            // 
+            this.idField.Location = new System.Drawing.Point(13, 13);
+            this.idField.Name = "idField";
+            this.idField.Size = new System.Drawing.Size(216, 20);
+            this.idField.TabIndex = 0;
+            this.idField.TextChanged += new System.EventHandler(this.IDField_TextChanged);
+            // 
+            // getAvatarButton
+            // 
+            this.getAvatarButton.Enabled = false;
+            this.getAvatarButton.Location = new System.Drawing.Point(235, 13);
+            this.getAvatarButton.Name = "getAvatarButton";
+            this.getAvatarButton.Size = new System.Drawing.Size(98, 20);
+            this.getAvatarButton.TabIndex = 1;
+            this.getAvatarButton.Text = "Get user avatar";
+            this.getAvatarButton.UseVisualStyleBackColor = true;
+            this.getAvatarButton.Click += new System.EventHandler(this.GetAvatarButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 274);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Waiting for ID input...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 332);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 352);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.getAvatarButton);
+            this.Controls.Add(this.idField);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Discord Avatar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox idField;
+        private System.Windows.Forms.Button getAvatarButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DiscordAvatar/DiscordAvatar/Menu.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Net.Http;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+using Newtonsoft.Json;
+
+namespace DiscordAvatar
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private HttpClient client;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", "Bot BOT_TOKEN_HERE");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void IDField_TextChanged(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(idField.Text, @"^\d{18}"))
+            {
+                label1.ForeColor = Color.Red;
+                label1.Text = "ID should contain only digits!";
+                getAvatarButton.Enabled = false;
+                return;
+            }
+
+            label1.ForeColor = Color.Green;
+            label1.Text = "Valid ID!";
+            getAvatarButton.Enabled = true;
+        }
+
+        private async void GetAvatarButton_Click(object sender, EventArgs e)
+        {
+            string content = client.GetStringAsync($"https://discord.com/api/v6/users/{idField.Text}").Result;
+            DiscordUser userObject = JsonConvert.DeserializeObject<DiscordUser>(content);
+
+            label2.Text = $"Avatar: {userObject.Username}#{userObject.Discriminator}";
+            pictureBox1.LoadAsync($"https://cdn.discordapp.com/avatars/{idField.Text}/{userObject.AvatarHash}.png?size=1024");
+
+            await DownloadAvatarLocally(userObject);
+        }
+
+        private async Task DownloadAvatarLocally(DiscordUser userObject)
+        {
+            var fs = new FileStream($"{userObject.AvatarHash}.png", FileMode.Create, FileAccess.Write);
+            var imgStream = await client.GetStreamAsync($"https://cdn.discordapp.com/avatars/{idField.Text}/{userObject.AvatarHash}.png?size=1024");
+            await imgStream.CopyToAsync(fs);
+
+            imgStream.Close();
+            fs.Close();
+        }
+    }
+}
